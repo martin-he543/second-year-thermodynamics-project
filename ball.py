5393384244b0257dda01fa4320566d2bb547b6de
Martin He <41821907+martin-he543@users.noreply.github.com>
2022-11-13 18:30:38 +0000

    Updated ball and simulation classes

--- ball.py
+++ ball.py
@@ -2,21 +2,140 @@ import numpy as np
 from copy import deepcopy
 
 class Ball:
+    time = 0
     n_ball = 0
     def __init__(
         self,
         m_ball=1,
         r_ball=1,
-        pos_ball=np.array([0,0]),
-        vel_ball=np.array([0,0]),
+        pos_ball=np.array([0.0,0.0]),
+        vel_ball=np.array([0.0,0.0]),
+        n_collision=0
     ):
-        self.__m_ball = m_ball
-        self.__r_ball = r_ball
-        self.__pos_ball = pos_ball
-        self.__vel_ball = vel_ball
+        self._m_ball = m_ball
+        self._r_ball = r_ball
+        self._pos_ball = pos_ball
+        self._vel_ball = vel_ball
+        self._n_collision = n_collision
+        self._dp = 0
         n_ball += 1
         return
+    
+    ### BALL INFORMATION METHODS
+    
+    def __repr__(self):
+        return ("Ball properties: mass = {self._m_ball}, radius = {self._r_ball}, position = {self._pos_ball}, velocity = {self._vel_ball}")
+    def __str__(self):
+        return ("Ball: m = {self._m_ball}; r = {self._r_ball}; x = {self._pos_ball}; v = {self._vel_ball}")
 
+    ### BALL MOVEMENT METHODS
+    
+    def time_to_collision(self,other):
+        """
+        Calculate the time until the next collision between this ball and another one, or the container.
+        """
+        r = self._pos_ball - other._pos_ball
+        v = self._vel_ball - other._vel_ball
+        
+        a = np.dot(v,v)
+        b = 2 * np.dot(r,v)
+        
+        
+        if a == 0:  # both balls have the same velocity directional vector
+            return np.inf
+        else:
+            if isinstance(other,Container):
+                R = self._r_ball - other.radius
+            else:
+                R = self._r_ball + other.radius
+            c = np.dot(r,r) - R**2
+            
+            if not isinstance(other,Container):
+                if np.abs(c) <= 10e-13: # floating point error
+                    return np.inf
+                
+            discrim = b**2 - 4*a*c
+            if discrim < 0: ## complex ðt
+                return np.inf
+            elif discrim == 0:
+                dt = -b/(2*a)
+                if dt <= 0:
+                    return np.inf
+                else:
+                    return dt
+            elif discrim > 0:
+                dt_1 = (-b + np.sqrt(discrim))/(2*a)
+                dt_2 = (-b - np.sqrt(discrim))/(2*a)
+                
+                if isinstance(other,Container):
+                    return np.amax(np.array([dt_1,dt_2]))
+                else:
+                    if dt_1 <= 0 and dt_2 <= 0:
+                        return np.inf
+                    elif dt_1 > 0 and dt_2 > 0:
+                        return np.amin(np.array([dt_1,dt_2]))
+                    elif dt_1 < 0:
+                        return dt_2
+                    elif dt_2 < 0:
+                        return dt_1
+
+    def collide(self, other):
+        """
+        Make the changes to the velocities of the ball, and the other one due to a collision.
+        RETURNS
+
+        """
+        r = self.__pos_ball - other._pos_ball
+        
+        u1_par = np.vdot(self._vel_ball, r) / np.dot(r,r) * r
+        u1_per = self._vel_ball - u1_par
+        
+        if isinstance(other,Container): # container collision
+            v1_par,v2_par = -u1_par, np.zeros(2)
+            v1_per = u1_per
+            self.set_vel(v1_par + v1_per)
+            other.set_vel(np.zeros(2))
+            self.count += 1
+            other.count = -1
+            
+            vel_f = self._vel_ball
+            vel_i = self._vel_ball
+            dv = vel_f - vel_i
+            self._dp = dv * self._m_ball
+            
+        else: # other ball collision
+            m1 = self._m_ball
+            m2 = other._m_ball
+            
+            u2_par,u2_per = np.vdot(other._vel_ball, r) / np.dot(r,r) * r, other._v_ball - u2_par
+            u1_par_translated = u1_par - u2_par
+            v1_par_translated = (m1 - m2)/(m1 + m2) * u1_par_translated
+            
+            v1_par = v1_par_translated + u2_par
+            v2_par = u1_par - u2_par + v1_par
+            v1_per = u1_per
+            v2_per = u2_per
+            self.set_vel(v1_par + v1_per)
+            other.set_vel(v2_par + v2_per)
+            self._count += 1
+            other._count += 1
+            
+    def move(self,dt):
+        """
+        Move the ball to a new position: 
+        r' = r + v * dt.
+        """
+        self.set_pos(self._pos + dt * self._vel)
+
+
+    ### BALL ATTRIBUTE METHODS
+    
+    def mass(self):
+        return self._m_ball
+    
+    def radius(self):
+        return self._r_ball
+    
     def pos(self):
         """
         Return the current position.
@@ -34,25 +153,37 @@ class Ball:
             the current 2D velocity of the ball.
         """
         return self.__vel_ball
-
-    def move(self,dt):
-        """
-        Move the ball to a new position: 
-        r' = r + v * dt.
-        """
-        self.__pos_ball = self.__pos_ball + (self.__vel_ball * dt)
-
-    def time_to_collision(self,r,v,R):
-        """
-        Calculate the time until the next collision between this ball and another one, or the container.
-        """
-        return -(np.dot(r,v)/np.dot(v,v)) + np.sqrt((np.dot(r,v))**2 - np.dot(v,v)(np.dot(r,r) - R*22))
-
-    def collide(self,other):
-        """
-        Make the changes to the velocities of the ball, and the other one due to a collision.
-        RETURNS
-
-        """
+    
+    def set_mass(self, mass):
+        self._m_ball = mass
+    
+    def set_radius(self,radius):
+        self._r_ball = radius
         
+    def set_pos (self, pos):
+        self._pos_ball = np.array(pos)
+    
+    def set_vel(self,vel):
+        self._vel_ball = np.array(vel)
+        
+    ### BALL OTHER METHODS
+    
+    def copy(self):
+        return deepcopy(self)
 
+
+class Container(Ball):
+    """
+    This is a Container class. The container is used to enclose the balls in
+    the 2D rigid disc collision.
+
+    Arguments:
+            radius (float): The radius of the container.
+            mass (float): The mass of the container.
+    """
+
+    def __init__(self, radius=10, mass=100):
+        super().__init__()
+        self._radius = radius
+        self._mass = mass
+        self._count = -1  # The collision count is always -1
--- simulation.py
+++ simulation.py
@@ -2,6 +2,66 @@ import numpy as np
 import ball as bl
 import matplotlib.pyplot as plt
 
+class Event(tuple):
+    """
+    A tuple of 5 elements (ball_A, ball_B, count_A, count_B, dt).
+
+    Parameters:
+        ball_A (int): The first ball in impending collision.
+        ball_B (int): The second ball in impending collision.
+        count_A (int): The number of collisions the first ball 
+            encountered prior to this impending collision calculation.
+        count_B (int): The number of collisions the second ball 
+            encountered prior to this impending collision calculation.
+        dt (float): The global time this collision will happen on.
+    """
+
+    def ball_A(self):
+        """
+        Returns:
+            (int): The index of first ball in impending collision.
+        """
+        return self[0]
+
+    def ball_B(self):
+        """
+        Returns:
+            (int): The index of second ball in impending collision.
+        """
+        return self[1]
+
+    def count_A(self):
+        """
+        Returns:
+            (int): The number of collisions the first ball 
+                encountered prior to this impending collision calculation.
+        """
+        return self[2]
+
+    def count_B(self):
+        """
+        Returns:
+            (int): The number of collisions the second ball 
+                encountered prior to this impending collision calculation.
+        """
+        return self[3]
+
+    def dt(self):
+        """
+        Returns:
+            (float): The global time this collision will happen on.
+        """
+        return self[4]
+
+    def pair(self):
+        """
+        Returns:
+            (list of int): A list of the two balls/ ball and container
+                involved in collision.
+        """
+        return [self[0], self[1]]
+
+
 class Simulation:
     """
     SIMULATION CLASS
@@ -17,13 +77,33 @@ class Simulation:
         self,
         N_balls=1,
         r_balls=1,
-        r_container=10,
+        m_balls=1,
+        r_container=10
     ):
         self.__N_balls=N_balls
         self.__r_balls=r_balls
         self.__r_container=r_container
-        return
+        self._ball=[]
+        self._temperature=[]
+        self._KE=[]
+        self._speed=[]
+        self._brownian=[]
+        
+        self._distance_centre=[]
 
+        for _ in range(0, N_balls):
+            self._ball.append(bl.Ball(radius=r_balls, mass=m_balls))
+
+
+    ### SIMULATION INFORMATION METHODS
+    
+    def __repr__(self):
+        return ("Simulation properties: N = {self._N_ball} balls, r_ball = {self._r_ball}, m_ball = {self._m_ball}, r_container = {self._r_container}")
+    def __str__(self):
+        return ("Simulation: N = {self._N_ball} balls, r_ball = {self._r_ball, m_ball = {self._m_ball}, r_container = {self._r_container}")
+    
+    ### SIMULATION MOVEMENT METHODS
+    
     def next_collision(self,ball,time):
         """
         Sets up and performs the next collision.
@@ -33,5 +113,51 @@ class Simulation:
         - move the system to that point in time
         - perform the collision
         """
-        ball=bl.Ball(1,2,[3,4],[5,6])
-        return ball.collide()
+
+    ### SIMULATION ATTRIBUTE METHODS
+    
+    def N_ball(self):
+        """
+        Returns:
+            (int): Number of balls in container.
+        """
+        return self._N_ball
+    
+    def ball(self):
+        return self._ball
+    
+    def container(self):
+        return self._container
+    
+    ### SIMULATION PROPERTY METHODS
+    
+    def pressure(self):
+        return self._pressure
+
+    def pressure_moyen(self):
+        return self._pressure_moyen
+    
+    def temperature(self):
+        return self._temperature
+    
+    def temperature_moyen(self):
+        return self._temperature_moyen
+    
+    def KE(self):
+        return self._KE
+    
+    def distance_centre(self):
+        return self._distance_centre
+    
+    def speed(self):
+        return self._speed
+    
+    def brownian(self):
+        return self._brownian
+    
+    def dataset(self):
+        return self._dataset
+    
+    ### SIMULATION OTHER METHODS
+    
+    
